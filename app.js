f9b8b7d8ffeb24c542d0ac9734bbe3465e8a818a
sydsquidster <89406043+sydsquidster@users.noreply.github.com>
2021-09-27 21:03:50 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,97 +1,32 @@
-var ele = document.body.querySelector(".loop") ;
-var user = document.body.querySelector(".user");
-var dragonhealth = document.body.querySelector(".dragonhealth");
+var user=document.body.querySelector(".user");
+var dragon=document.body.querySelector(".dragon");
+var userwinlose=document.body.querySelector(".userwinlose");
 
-var condition = true;
+var condition = true
+var userdamage = 0
+var dragondamage = 0
 
 while(condition) {
-   var hit = Number(prompt("How many hits will you attempt on the dragon?"));
-   if (hit < 5) {
-      hit = 1;
-   }
-   var rhit = Math.floor(Math.random() * hit) + 1;
-   var dragon = Math.floor(Math.random() * 2) + 1;
+    var hit = Number(prompt("How many hits would you like to attempt on the dragon?"));
+    var randomnum = Math.ceil(Math.random() * hit) ;
+    var dragonnum = Math.floor(Math.random() * 2) + 1;
 
-   var rhit2 = rhit + rhit;
-   var rhit3 = rhit2 + rhit;
-   var rhit4 = rhit3 +rhit;
-   var rhit5 = rhit4 +rhit;
+    if(randomnum>5){
+        randomnum= 1;
+    }
+    dragondamage = randomnum +dragondamage;
+    userdamage = dragonnum + userdamage;
 
-   var dragon2 = dragon + dragon;
-   var dragon3 = dragon2 + dragon;
-   var dragon4 = dragon3 + dragon;
-   var dragon5 = dragon4 + dragon;
-
-   if(rhit=10){
-      condition=false;
-       user.innerHTML =rhit;
-       dragonhealth.innerHTML = dragon;
-       ele.innerHTML = "User has won!";
-   }if(dragon=5){
-      condition=false;
-       user.innerHTML = rhit;
-       dragonhealth.innerHTML = dragon;
-       ele.innerHTML = "User has lost!";
-   }if(rhit2=10){
-   condition=false;
-    user.innerHTML = rhit2;
-     dragonhealth.innerHTML = dragon2;
-     ele.innerHTML = "User has won!";
-  } if(dragon2=5){
-   condition=false;
-    user.innerHTML = rhit2;
-    dragonhealth.innerHTML = dragon2;
-    ele.innerHTML = "User has lost!";
-  }if(rhit3=10) {
-     condition = false;
-      user.innerHTML = rhit3;
-      dragonhealth.innerHTML = dragon3;
-      ele.innerHTML = "User has won!";
-  }if(dragon3=5) {
-     condition = false;
-      user.innerHTML = rhit3;
-      dragonhealth.innerHTML = dragon3;
-      ele.innerHTML = "User has lost!";
-  }if(rhit4=10) {
-      condition = false;
-       user.innerHTML = rhit4;
-       dragonhealth.innerHTM = dragon4;
-       ele.innerHTML = "User has won!";
-  }if(dragon4=5){
-  condition = false;
-   user.innerHTML = rhit4;
-   dragonhealth.innerHTML = dragon4;
-   ele.innerHTML = "User has lost!";
-  }if(rhit5=10){
-      condition = false;
-       user.innerHTML = rhit5;
-       dragonhealth.innerHTM = dragon5;
-       ele.innerHTML = "User has won!";
-  }if(dragon5=5){
-      condition = false;
-      user.innerHTML = rhit5;
-      dragonhealth.innerHTML = dragon5;
-      ele.innerHTML = "User has lost!";
-   }}
-
-
-
-
-
-  // for(var i=0; i<5; i++ ){
-  //    userdamage = 0 + rhit;
-  //    dragondamage = 0 + dragon;
-   //}
-   //if(userdamage>10) {
-  //    condition = false;
-  //    user.innerHTML = userdamage;
-  //    dragonhealth.innerHTML = dragondamage;
-  //    ele.innerHTML = "User has won!";
-  // }
-  // else if(dragondamage>5){
-  //    condition = false;
-  //    user.innerHTML = userdamage;
-  //    dragonhealth.innerHTML = dragondamage;
-  //    ele.innerHTML = "User has lost!";
-
- //  }
+    if(dragondamage>=10){
+        condition = false;
+        user.innerHTML= "User Damage: " + userdamage;
+        dragon.innerHTML="Dragon Damage: " + dragondamage;
+        userwinlose.innerHTML= "User Wins!";
+    }
+    else if(userdamage>=5){
+        condition= false;
+        user.innerHTML="User Damage: " + userdamage;
+        dragon.innerHTML= "Dragon Damage: " +dragondamage;
+        userwinlose.innerHTML= "User Loses!";
+    }
+}
